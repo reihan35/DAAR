13a58419e689a1120dc7f559d6bbf493262bfa71
reihan35 <reihaneh77@yahoo.com>
2019-09-23 18:06:17 +0200

    kmp

---
+++ Kmp.java
@@ -0,0 +1,45 @@
+public class Kmp{
+    public static  int first_time(char[] f, char c){
+        for(int i=0; i<f.length;i++){
+            if(f[i]==c){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int[] retenu(char[] facteur){
+        int[] retenus = new int[facteur.length];
+        
+        for(int i = 0; i<facteur.length;i++){
+            if(facteur[i] == facteur[0]){
+                retenus[i] = -1;
+            }
+            else{
+                if(i-1 == first_time(facteur, facteur[i-1])){
+                    System.out.println("je suis ");
+                    retenus[i] = 0;
+                }
+                else{
+                    System.out.println( "i = " + i + "firsttime " + first_time(facteur, facteur[i-1]) + "facteur i-1 " + facteur[i-1] );
+                    retenus[i] = (i-1)-first_time(facteur, facteur[i-1]) - 1;
+                }
+            }
+        }
+        //retenus[1] = 0;
+        return retenus;
+    }
+
+   public static void main(String[] args){
+       // char[] facteur = {'m','a','m','a','m','i','a'}; 
+        char[] facteur = {'c','h','i','c','h','a'}; 
+
+        int [] result = retenu(facteur);
+        for(int i=0; i< result.length; i++)
+            System.out.print(result[i] + ", ");
+    } 
+}
+
+/*public int mathcingALgo(){
+
+}*/
